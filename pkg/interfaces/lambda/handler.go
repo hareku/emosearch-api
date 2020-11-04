ff69b75c589ff2bb030870ac243802fdaad528c6
hareku <hareku908@gmail.com>
2020-11-04 21:51:12 +0000

    Tweak handler

--- pkg/interfaces/lambda/handler.go
+++ pkg/interfaces/lambda/handler.go
@@ -41,20 +41,9 @@ func (h *handler) registerRoutes() {
 	h.router.Route("GET", "/@me", fetchMe)
 }
 
-type fetchMeInput struct {
-	Authorization string `lambda:"header.Authorization"`
-}
-
 func fetchMe(ctx context.Context, req events.APIGatewayProxyRequest) (
 	res events.APIGatewayProxyResponse,
 	err error,
 ) {
-	var input fetchMeInput
-	err = lmdrouter.UnmarshalRequest(req, false, &input)
-	if err != nil {
-		return lmdrouter.HandleError(err)
-	}
-
-	var data interface{}
-	return lmdrouter.MarshalResponse(http.StatusOK, nil, data)
+	return lmdrouter.MarshalResponse(http.StatusOK, nil, "Hello world")
 }
