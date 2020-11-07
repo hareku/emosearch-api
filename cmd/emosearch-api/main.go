5f0f97b8a4c8954e092e4505c720420beb2da6b7
hareku <hareku908@gmail.com>
2020-11-07 06:33:03 +0000

    Rename lambda dir

--- cmd/emosearch-api/main.go
+++ cmd/emosearch-api/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/hareku/emosearch-api/pkg/interfaces/lambda"
+	"github.com/hareku/emosearch-api/pkg/interfaces/lambda/api"
 	"github.com/hareku/emosearch-api/pkg/registry"
 )
 
 func main() {
 	registry := registry.NewRegistry()
-	handler := lambda.NewLambdaHandler(registry)
+	handler := api.NewLambdaHandler(registry)
 	handler.Start()
 }
--- pkg/interfaces/lambda/api/handler.go
+++ pkg/interfaces/lambda/api/handler.go
@@ -1,4 +1,4 @@
-package lambda
+package api
 
 import (
 	"net/http"
