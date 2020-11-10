ccaf49191ad104c39a1993eb4612ab0c5ac9a9a4
hareku <hareku908@gmail.com>
2020-11-10 13:52:48 +0000

    Update tweet struct of dynamo

--- pkg/infrastructure/dynamodb/tweet_repository.go
+++ pkg/infrastructure/dynamodb/tweet_repository.go
@@ -9,7 +9,6 @@ import (
 	"github.com/guregu/dynamo"
 	"github.com/hareku/emosearch-api/pkg/domain/model"
 	"github.com/hareku/emosearch-api/pkg/domain/repository"
-	"github.com/hareku/emosearch-api/pkg/domain/sentiment"
 )
 
 type dynamoDBTweetRepository struct {
@@ -24,36 +23,11 @@ func NewDynamoDBTweetRepository(dynamoDB dynamo.Table) repository.TweetRepositor
 type dynamoDBTweet struct {
 	PK string
 	SK string
-
-	TweetID                model.TweetID
-	SearchID               model.SearchID
-	AuthorID               int64
-	Text                   string
-	SentimentScoreMixed    *float64
-	SentimentScoreNegative *float64
-	SentimentScoreNeutral  *float64
-	SentimentScorePositive *float64
-	TweetCreatedAt         time.Time
-	CreatedAt              time.Time
-	UpdatedAt              time.Time
+	*model.Tweet
 }
 
 func (d *dynamoDBTweet) NewTweetModel() *model.Tweet {
-	return &model.Tweet{
-		TweetID:  d.TweetID,
-		SearchID: d.SearchID,
-		AuthorID: d.AuthorID,
-		Text:     d.Text,
-		SentimentScore: &sentiment.Score{
-			Negative: d.SentimentScoreNegative,
-			Positive: d.SentimentScorePositive,
-			Mixed:    d.SentimentScoreMixed,
-			Neutral:  d.SentimentScoreNeutral,
-		},
-		TweetCreatedAt: d.TweetCreatedAt,
-		CreatedAt:      time.Time(d.CreatedAt),
-		UpdatedAt:      time.Time(d.UpdatedAt),
-	}
+	return d.Tweet
 }
 
 func (r *dynamoDBTweetRepository) Store(ctx context.Context, tweet *model.Tweet) error {
@@ -62,20 +36,9 @@ func (r *dynamoDBTweetRepository) Store(ctx context.Context, tweet *model.Tweet)
 	tweet.UpdatedAt = createdAt
 
 	dtweet := dynamoDBTweet{
-		PK: fmt.Sprintf("SEARCH#%s", tweet.SearchID),
-		SK: fmt.Sprintf("TWEET#%d", tweet.TweetID),
-
-		TweetID:                tweet.TweetID,
-		AuthorID:               tweet.AuthorID,
-		SearchID:               tweet.SearchID,
-		Text:                   tweet.Text,
-		SentimentScoreMixed:    tweet.SentimentScore.Mixed,
-		SentimentScoreNegative: tweet.SentimentScore.Negative,
-		SentimentScoreNeutral:  tweet.SentimentScore.Negative,
-		SentimentScorePositive: tweet.SentimentScore.Neutral,
-		TweetCreatedAt:         tweet.TweetCreatedAt,
-		CreatedAt:              tweet.CreatedAt,
-		UpdatedAt:              tweet.CreatedAt,
+		PK:    fmt.Sprintf("SEARCH#%s", tweet.SearchID),
+		SK:    fmt.Sprintf("TWEET#%d", tweet.TweetID),
+		Tweet: tweet,
 	}
 
 	err := r.dynamoDB.Put(&dtweet).RunWithContext(ctx)
