d078efca261e76476302e8c993824f5ef1c4efca
hareku <hareku908@gmail.com>
2020-12-21 17:49:19 +0000

    update to use bayes api instead of comprehend

--- pkg/registry/sentiment.go
+++ pkg/registry/sentiment.go
@@ -1,26 +1,10 @@
 package registry
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/comprehend"
 	"github.com/hareku/emosearch-api/pkg/domain/sentiment"
-	_comprehend "github.com/hareku/emosearch-api/pkg/infrastructure/comprehend"
+	"github.com/hareku/emosearch-api/pkg/infrastructure/bayes"
 )
 
-var comprehendClient *comprehend.Comprehend
-
-func getComprehendClient() *comprehend.Comprehend {
-	if comprehendClient == nil {
-		awsConf := &aws.Config{
-			Region: aws.String("ap-northeast-1"),
-		}
-		comprehendClient = comprehend.New(session.Must(session.NewSession()), awsConf)
-	}
-
-	return comprehendClient
-}
-
 func (r *registry) NewSentimentDetector() sentiment.Detector {
-	return _comprehend.NewComprehendDetector(getComprehendClient())
+	return bayes.NewBayesDetector()
 }
